afa04a73df08a8d94a1d11dc6a73c9e5337a340a
Mehrshad <lastenigmas@gmail.com>
2023-03-03 13:24:43 +0330

    ready for showing her!

--- HaniehV1/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace HaniehV1.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
